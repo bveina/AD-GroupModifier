56bff50a96f0a1893d52b28e2465b34933a746a1
Ben Viall <bveina@users.noreply.github.com>
2020-08-07 11:07:15 -0400

    initial file dump

---
+++ AD_GroupAdd/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace AD_GroupAdd
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "SENG-214-users",
+            "SENG-106-users"});
+            this.comboBox1.Location = new System.Drawing.Point(13, 13);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(125, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.Text = "SENG-214-users";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            this.comboBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.comboBox1_KeyDown);
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtUserName.Location = new System.Drawing.Point(13, 41);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(125, 20);
+            this.txtUserName.TabIndex = 1;
+            this.txtUserName.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtUserName_KeyDown);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnAdd.Location = new System.Drawing.Point(13, 67);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(125, 23);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.DisplayMember = "name";
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(13, 97);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(125, 95);
+            this.listBox1.TabIndex = 3;
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnRemove.Location = new System.Drawing.Point(13, 198);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(125, 23);
+            this.btnRemove.TabIndex = 4;
+            this.btnRemove.Text = "Remove";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(150, 233);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ AD_GroupAdd/Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.DirectoryServices;
+using System.DirectoryServices.AccountManagement;
+
+namespace AD_GroupAdd
+{
+    public partial class Form1 : Form
+    {
+        string MyDomain;
+        public Form1()
+        {
+            InitializeComponent();
+            MyDomain = "UMDAR.umassd.edu";
+            timer1.Interval = 1000;
+            
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            txtUserName.Focus();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            updateList();
+        }
+
+
+        public void updateList()
+        {
+            if (comboBox1.Text == "") return;
+            PopulateUsers(this.MyDomain, comboBox1.Text);
+            
+        }
+        /*
+
+        //https://stackoverflow.com/questions/7915145/get-all-users-from-a-group-in-active-directory
+        public static SearchResultCollection GetListOfAdUsersByGroup(string domainName, string groupName)
+        {
+            DirectoryEntry entry = new DirectoryEntry("LDAP://UMDAR.umassd.edu");
+            DirectorySearcher search = new DirectorySearcher(entry);
+            string query = "(&(objectCategory=person)(objectClass=user)(memberOf=\"ii-214-users\"))";
+            search.Filter = query;
+            search.PropertiesToLoad.Add("memberOf");
+            search.PropertiesToLoad.Add("name");
+
+            return search.FindAll();
+        }
+        */
+        //https://stackoverflow.com/questions/2143052/adding-and-removing-users-from-active-directory-groups-in-net#2143742
+        public void AddUserToGroup(string domainName, string userId, string groupName)
+        {
+            try
+            {
+                using (PrincipalContext pc = new PrincipalContext(ContextType.Domain, domainName))
+                {
+                    GroupPrincipal group = GroupPrincipal.FindByIdentity(pc, groupName);
+                    group.Members.Add(pc, IdentityType.SamAccountName, userId);
+                    group.Save();
+                }
+            }
+            catch (UnauthorizedAccessException e)
+            {
+                MessageBox.Show("Im sorry dave im afaid you cant do that",e.Message);
+            }
+            catch (System.DirectoryServices.DirectoryServicesCOMException E)
+            {
+                //doSomething with E.Message.ToString(); 
+                MessageBox.Show("Im sorry dave im afaid you cant do that", E.Message);
+
+            }
+        }
+
+        public void RemoveUserFromGroup(string domainName, string userId, string groupName)
+        {
+            if (userId=="")
+            {
+                MessageBox.Show("BlankUsername");
+            }
+            try
+            {
+                using (PrincipalContext pc = new PrincipalContext(ContextType.Domain, domainName))
+                {
+                    GroupPrincipal group = GroupPrincipal.FindByIdentity(pc, groupName);
+                    group.Members.Remove(pc, IdentityType.SamAccountName, userId);
+                    group.Save();
+                }
+            }
+            catch (UnauthorizedAccessException e)
+            {
+                MessageBox.Show("Im sorry dave im afaid you cant do that", e.Message);
+            }
+            catch (System.DirectoryServices.DirectoryServicesCOMException E)
+            {
+                //doSomething with E.Message.ToString(); 
+
+            }
+        }
+
+        public void PopulateUsers(string domainName, string groupName)
+        {
+            using (var context = new PrincipalContext(ContextType.Domain, domainName))
+            {
+                using (var group = GroupPrincipal.FindByIdentity(context, groupName))
+                {
+                    
+                    if (group == null)
+                    {
+                        MessageBox.Show("Group does not exist");
+                        timer1.Enabled = false;
+                        return;
+                    }
+                    else
+                    {
+                        PrincipalSearchResult<Principal> users = group.GetMembers(false);
+                        listBox1.SuspendLayout();
+                        int sel = listBox1.SelectedIndex;
+                        listBox1.Items.Clear();
+                        foreach (Principal v in users)
+                        {
+                            //if (v.Name.Contains("ECE-")) continue;
+                            //if (listBox1.Items.Contains(v)) continue;
+                            listBox1.Items.Add(v);
+                        }
+                        listBox1.SelectedIndex = sel;
+                        listBox1.ResumeLayout();
+
+                    }
+                }
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (comboBox1.Focused) return;
+            updateList();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (txtUserName.Text=="")
+            {
+                MessageBox.Show("Blank username?");
+                return;
+            }
+            AddUserToGroup(MyDomain, txtUserName.Text, comboBox1.Text);
+            txtUserName.Text = "";
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex==-1)
+            {
+                MessageBox.Show("select a user to remove");
+                return;
+            }
+            RemoveUserFromGroup(MyDomain, (listBox1.SelectedItem as Principal).SamAccountName, comboBox1.Text);
+
+        }
+
+        private void comboBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            
+            if (e.KeyCode == Keys.Enter)
+            {
+                //todo: check if group exists
+            }
+        }
+
+        private void txtUserName_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                if (txtUserName.Text == "")
+                {
+                    MessageBox.Show("Blank username?");
+                    return;
+                }
+                AddUserToGroup(MyDomain, txtUserName.Text, comboBox1.Text);
+                txtUserName.Text = "";
+            }
+        }
+    }
+}
+    
